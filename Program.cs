7aeb4e14e3220b974cf296d4bd299256cb17fefb
Ponikarovskiy <ponikarovsky.a@gmail.com>
2023-02-17 19:19:04 +0300

    Решение задачи № 34

--- Program.cs
+++ Program.cs
@@ -1,6 +1,44 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+Console.Clear();
+System.Console.WriteLine();
+System.Console.WriteLine("*Задача № 34. Найти чётные числа в массиве*");
+System.Console.WriteLine();
+System.Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+System.Console.WriteLine("Текущий массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+    if (numbers[z] % 2 == 0)
+        count++;
+
+System.Console.WriteLine($"Всего чисел - {numbers.Length}");
+System.Console.WriteLine($"Из них четных - {count}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100, 1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    System.Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + ", ");
+    }
+    System.Console.WriteLine("]");
+    System.Console.WriteLine();
+}
+
+
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
