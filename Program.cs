1bbc9515af59e39457758227b1b41cd4db4eee49
Ponikarovskiy <ponikarovsky.a@gmail.com>
2023-02-17 19:30:03 +0300

    Решение задачи № 36

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
-
+/*
 Console.Clear();
 System.Console.WriteLine();
 System.Console.WriteLine("*Задача № 34. Найти чётные числа в массиве*");
@@ -37,11 +37,46 @@ void PrintArray(int[] numbers)
     System.Console.WriteLine("]");
     System.Console.WriteLine();
 }
-
+*/
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+System.Console.WriteLine("*Задача № 36. Найти сумму элементов на нечетных позициях в массиве*");
+System.Console.WriteLine();
+System.Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+System.Console.WriteLine("Текущий массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int z = 0; z < numbers.Length; z+=2)
+    sum = sum + numbers[z];
+
+    System.Console.WriteLine($"Всего чисел - {numbers.Length}");
+    System.Console.WriteLine($"Сумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    System.Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            System.Console.Write(numbers[i] + " ");
+        }
+    System.Console.Write("]");
+    System.Console.WriteLine();
+}
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
